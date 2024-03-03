ed8082ee265b858c275b61e2b48574e8fa311ff9
Vlad <seregaduru3000@mail.ru>
2024-03-03 21:56:40 +0300

    Rebuilt the task settings component, made task creation and the ability to delete it

--- src/components/TaskCard/index.vue
+++ src/components/TaskCard/index.vue
@@ -2,49 +2,12 @@
   <div
       v-if="created == true"
       class="tab-content-projects-slider-card-tasks__block tab-content-projects-slider-card-tasks-block create"
-      @click="openCheck = true"
+      v-bind="$attrs"
   >
     <span class="material-symbols-outlined">add</span>
     <span>Добавить</span>
-
   </div>
-    <div class="task-created-block" :class="{open: openCheck}">
-      <span class="task-created-block__header task-created-block-header">TManager</span>
-      <div class="button-modal-close" @click="exitCreateTask()">Закрыть</div>
-      <hr>
-      <div class="modal-settings-block">
-        <div class="modal-settings-block__line modal-settings-block-line">
-          <span class="modal-settings-block__title">Наименование задачи: </span>
-          <input v-model="titleTask" class="modal-settings-block__input" placeholder="-">
-        </div>
-        <div class="modal-settings-block__line modal-settings-block-line">
-          <span class="modal-settings-block__title">Владелец: </span>
-          <div class="modal-settings-block__input disabled"> {{ login }}</div>
-        </div>
-        <div class="modal-settings-block__line modal-settings-block-line">
-          <span class="modal-settings-block__title">Исполнитель: </span>
-          <input v-model="developerTask" class="modal-settings-block__input" placeholder="-">
-        </div>
-        <div class="modal-settings-block__line modal-settings-block-line">
-          <span class="modal-settings-block__title">Время разработки: </span>
-          <input v-model="developerTimeTask" class="modal-settings-block__input" placeholder="-">
-        </div>
-        <div class="modal-settings-block__line modal-settings-block-line">
-          <span class="modal-settings-block__title">Дата сдачи: </span>
-          <input v-model="timeTask" class="modal-settings-block__input" placeholder="-">
-        </div>
-        <div class="modal-settings-block__line modal-settings-block-line">
-          <span class="modal-settings-block__title">Приоритет: </span>
-          <input v-model="priorityTask" class="modal-settings-block__input" placeholder="-">
-        </div>
-        <hr/>
-        <div class="modal-settings-block__line modal-settings-block-line">
-          <span class="modal-settings-block__title">Описание: </span>
-          <textarea v-model="descriptionTask" class="modal-settings-block__input" rows="15"  placeholder="-"></textarea>
-        </div>
-        <button class="button-create-task" @click="createTask()">Создать</button>
-      </div>
-    </div>
+    <slot/>
   <div
       v-if="created == false"
       class="tab-content-projects-slider-card-tasks__block tab-content-projects-slider-card-tasks-block"
@@ -95,7 +58,7 @@
           class="tab-content-projects-slider-card-tasks-block-description__context tab-content-projects-slider-card-tasks-block-description-context">Приоритет:</span>
       <span
           class="tab-content-projects-slider-card-tasks-block-description__value tab-content-projects-slider-card-tasks-block-description-value">
-        {{ prioritet }} / 10
+        {{ rating }} / 10
       </span>
     </div>
     <div class="tab-content-projects-slider-card-tasks-block-description">
@@ -110,11 +73,6 @@
 </template>
 
 <script lang="ts">
-import { storeToRefs } from 'pinia'
-import { useTaskStore } from '@/stores/tasks'
-
-const store = useTaskStore()
-const { tasks } = storeToRefs(store)
 export default {
   props: {
     created: Boolean,
@@ -123,56 +81,8 @@ export default {
     owner: String,
     timeDevelopment: String,
     timeEnd: String,
-    prioritet: Number,
+    rating: Number,
     status: String
-  },
-  data() {
-    return {
-      login: 'Владислав Шалаев',
-      openCheck: false,
-      titleTask: '',
-      developerTask: '',
-      developerTimeTask: '',
-      timeTask: '',
-      priorityTask: '',
-      descriptionTask: ''
-    }
-  },
-  methods: {
-    exitCreateTask() {
-      this.openCheck = false
-      this.titleTask = ''
-      this.developerTask = ''
-      this.developerTimeTask = ''
-      this.timeTask = ''
-      this.priorityTask = ''
-      this.descriptionTask = ''
-    },
-
-    createTask() {
-      const timeStamp = Date.now()
-
-      const task = {
-        sectionId: 1,
-        id: timeStamp,
-        title: this.titleTask,
-        developer: this.developerTask,
-        owner: this.login,
-        timeDevelop: this.developerTimeTask,
-        timeEnd: this.timeTask,
-        prior: this.priorityTask
-      }
-
-      store.addTask(task)
-
-      this.openCheck = false
-      this.titleTask = ''
-      this.developerTask = ''
-      this.developerTimeTask = ''
-      this.timeTask = ''
-      this.priorityTask = ''
-      this.descriptionTask = ''
-    }
   }
 }
 </script>
