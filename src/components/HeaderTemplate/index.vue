567a916e81e336dae1e69544ae95c10453ce03c4
Vlad <seregaduru3000@mail.ru>
2024-03-04 17:08:36 +0300

    refactor: style real time in header

--- src/components/HeaderTemplate/index.vue
+++ src/components/HeaderTemplate/index.vue
@@ -27,7 +27,10 @@ export default {
   methods: {
     getTime: function() {
       const today = new Date();
-      this.timeNow = today.getHours() + ":" + today.getMinutes();
+      const hours = String(today.getHours()).padStart(2, '0');
+      const minutes = String(today.getMinutes()).padStart(2, '0');
+      const seconds = String(today.getSeconds()).padStart(2, '0');
+      this.timeNow = hours + ":" + minutes + ":" + seconds;
     }
   }
 }
